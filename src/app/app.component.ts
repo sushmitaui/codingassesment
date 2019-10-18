bf201df9b30a3d702169ba7ae30f5b39dd457946
sushmita <sushmita@gmail.com>
2019-10-18 14:21:17 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  title = "coding-test";
+  remainingAmout = 167;
+  remainingDays = 3;
+  otherdonarcount = 42;
+  whyGiveAmount = 50;
+}
